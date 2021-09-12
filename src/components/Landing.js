7e857b0020d70119951af1a20aa4009a921fa08f
Devante Lowery <dlowery2120@gmail.com>
2021-09-12 10:22:52 -0500

    added withRouter to landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import {withRouter} from 'react-router-dom';
 
 const Landing = (props) => {
     return (
         <div>
-
+            <h1>Landing</h1>
         </div>
     )
 }
 
-export default Landing
+export default withRouter(Landing)
